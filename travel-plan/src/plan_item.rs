67eb345272010be352642a95992e5d7d4657c6af
ziv-y-chang <ziv_y_chang@trendmicro.com>
2025-08-22 15:35:07 +0800

    try enum with arg'

--- travel-plan/src/plan_item.rs
+++ travel-plan/src/plan_item.rs
@@ -6,22 +6,21 @@ use crate::cost::Cost;
 
 
 #[derive(Debug, Deserialize, Serialize)]
-pub enum PlanItemType {
-    Accomodation,
-    Transport,
-    Food,
-    Site,
-    Document,
-}
-
-#[derive(Debug, Deserialize, Serialize)]
-pub struct PlanItem {
-    pub item_type: PlanItemType,
+pub struct Item {
     pub name: String,
     pub description: String,
     pub from: DateTime<Utc>,
     pub to: DateTime<Utc>,
     pub cost: Cost,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub enum PlanItem {
+    Accomodation(Item),
+    Transport(Item),
+    Food(Item),
+    Site(Item),
+    Document(Item),
 }
 
 #[cfg(test)]
@@ -33,17 +32,16 @@ mod tests {
 
     use support::data_storage::{save_bin, read_bin};
 
-    use crate::plan_item::{PlanItem, PlanItemType};
+    use crate::plan_item::{PlanItem, Item};
     #[test]
     fn show_struct() {
-        let plan_item = PlanItem{
-            item_type: PlanItemType::Transport,
+        let plan_item = PlanItem::Transport(Item{
             name: String::from("test_item"),
             description: String::from("test_item desc"),
             from: Utc::now(),
             to: Utc::now(),
             cost: Cost{cost:-1, currency: Currency::USD},
-        };
+        });
         print!("{:?} \n", plan_item);
 
         save_bin("../test_data/save_plan_item.bin", plan_item);
